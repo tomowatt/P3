beb362f12e56c9d5221fe482150fc430f0d15295
Tom Watt <tomwatt@Tom-Book-Pro.lan>
2020-08-05 09:02:18 +0100

    Setup Golang Application trying to mirror Python in structure * JSON encoding still needs work as not initially detected as Raw JSON in Firefox * Needs more/refactored error handling * Needing work on use of `var` vs `:=`

---
+++ golang/src/app.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+type RandomPassword struct {
+	Password string
+}
+
+func getWords() []string {
+	var file, error = os.Open("nouns.txt")
+
+	if error != nil {
+		fmt.Println(error.Error())
+	}
+	defer file.Close()
+
+	var words []string
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		words = append(words, scanner.Text())
+	}
+	return words
+}
+
+func getRandomString(wordCount int) string {
+	var words = getWords()
+	var randomWords []string
+
+	for i := 0; i < wordCount; i++ {
+		rand.Seed(time.Now().UnixNano())
+		randomWords = append(randomWords, words[rand.Intn(len(words))])
+	}
+
+	var randomWordString = strings.Join(randomWords, "-")
+
+	return randomWordString
+}
+
+func onGet(writer http.ResponseWriter, request *http.Request) {
+	var password = RandomPassword{getRandomString(5)}
+	data, error := json.Marshal(password)
+
+	if error != nil {
+		panic(error)
+	}
+
+	fmt.Fprint(writer, string(data))
+}
+
+func main() {
+	var port = 80
+	http.HandleFunc("/", onGet)
+	http.ListenAndServe(fmt.Sprintf(":%v", port), nil)
+}
