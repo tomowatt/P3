f30d6066033782a229366ded06825f006e4848ae
Tom Watt <tomwatt@Tom-Book-Pro.lan>
2020-08-07 09:02:27 +0100

    Updated Python with Static Typing for ease of comparison between other languages

--- python/src/app.py
+++ python/src/app.py
@@ -6,19 +6,18 @@ import falcon
 
 class PasswordString:
 
-    def get_words(self):
+    def get_words(self) -> str:
         with open('nouns.txt') as nouns:
             return nouns.read().splitlines()
 
-    def get_random_string(self, word_count=5):
+    def get_random_string(self, word_count: int) -> str:
         words = self.get_words()
         random_word_string = '-'
         random_words = [word for word in random.choices(words, k=word_count)]
         return random_word_string.join(random_words)
 
     def on_get(self, request, response):
-        """Handles GET requests"""
-        data = {"Password": self.get_random_string()}
+        data = {"Password": self.get_random_string(5)}
         response.media = data
 
 api = falcon.API()
