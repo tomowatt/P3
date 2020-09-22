b0e76df3a8f12b563a2170ec9c742b3b086f97ae
tom <tomowatt@me.com>
2020-09-22 09:45:42 +0100

    Ruby Added
    * Found this similar to writing the Python application - Gemfile vs requirements, simplified file open & looping, etc
    * Again using the individual steps helped ease porting into Ruby, though still referred back for structure and method naming
    * Using `sinatra` reminded me of `flask` in Python, being light-weight and easily configurable

---
+++ ruby/src/app.rb
@@ -0,0 +1,48 @@
+require 'sinatra'
+
+
+def get_words ()
+  nouns = Array.new
+  # Open a File
+  File.open('nouns.txt', 'r') do |file|
+    # Read File contents
+    file.each do |line|
+      # Put contents into an Array/List/Vector
+      nouns.push(line.strip)
+    end
+  file.close
+  end
+  return nouns
+end
+
+def get_random_string (word_count)
+  words = get_words()
+  random_words = Array.new
+  for i in 0..word_count - 1
+    # Select N number of items from Array/List/Vector at Random
+    random_words.push(words[rand(words.length)])
+  end
+  # Generate a String from concatenating the Randomly selecting items
+  random_words_string = random_words.join('-')
+  return random_words_string
+end
+
+def on_get ()
+   # Create JSON object using Generated String
+   data = {:Password => get_random_string(5)}
+   # Return JSON object on GET requests
+   response = JSON.generate(data)
+   return response
+end
+
+# Host a Web application
+configure do
+  set :bind, '0.0.0.0'
+  set :host, '*'
+  set :port, '80'
+  set :default_content_type, 'application/json'
+end
+
+get '/' do
+  on_get()
+end
